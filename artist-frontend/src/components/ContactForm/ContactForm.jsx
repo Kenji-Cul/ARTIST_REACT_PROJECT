2d0c37717a1ecedfd5aff441c9e1cf7d3bae9ec8
Gideon Teibo <gideonteibo@gmail.com>
2024-04-30 17:04:26 +0100

    git commit -a

--- artist-frontend/src/components/ContactForm/ContactForm.jsx
+++ artist-frontend/src/components/ContactForm/ContactForm.jsx
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ContactForm.css'
 
 const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [location, setLocation] = useState('');
+  const [email, setEmail] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+    const contacts = {name, location, email, number};
+
+    console.log(contacts);
+  }
+
+
   return (
     <div className="contact-form-container">
            <div className="form-container">
             <h3>Contact Us</h3>
 
-         <form action="" className="contact-form">
-            <input type="text" placeholder="Name" />
-            <input type="text" placeholder="Location" />
-            <input type="email" placeholder="Email" />
-            <input type="number" placeholder="Phone" />
+         <form  className="contact-form" onSubmit={handleSubmit}>
+            <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} required/>
+            <input type="text" placeholder="Location" value={location} onChange={(e) => setLocation(e.target.value)} required/>
+            <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+            <input type="number" placeholder="Phone" value={number} onChange={(e) => setNumber(e.target.value)} required/>
             <button type="submit">Send Message</button>
          </form>
          </div>
